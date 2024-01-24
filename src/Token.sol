f349c1dd2a94d3834ec6b3b692d4fa2ee2051259
mr-abims <adebayoabimbola9@gmail.com>
2024-01-24 21:15:55 +0100

    update

--- src/Token.sol
+++ src/Token.sol
@@ -20,6 +20,13 @@ contract Token is ERC20 {
     function mint(address to, uint256 amount) public onlyOwner {
         _mint(to, amount);
     }
+     function balanceOf(address user) public override view returns (uint256) {
+       return super.balanceOf(user);
+    }
+
+    function transfer(address receiver, uint256 amount) public override returns (bool) {
+        return super.transfer(receiver, amount);
+    }
 
 // Function to burn tokens. Any user can burn their own tokens.
     function burn(uint256 amount) public {
